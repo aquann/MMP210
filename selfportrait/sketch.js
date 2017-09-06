36d1e6d17ed1cab9031ddaf5a357b47e76c059df
aquann <alexander.quann@f1006-30-34172.bmcc.cuny.edu>
2017-09-05 22:44:44 -0400

    final upload

--- selfportrait/sketch.js
+++ selfportrait/sketch.js
@@ -2,20 +2,21 @@ function setup() {
 createCanvas (640, 460);
 }
 function draw() {
- background("white"); 
+    background("white"); 
     noStroke();
-fill("saddlebrown");
+    fill("saddlebrown");
     
-ellipse(190, 220, 300, 350) ;
+    ellipse(125, 220, 300, 350) ;
+    
     fill(180, 50, 250, 127)
-ellipse(150,200,50,100);
+    ellipse(150,200,50,100);
     ellipse(250,200,50,100);
     
-fill(200)   
+    fill(200)   
     
 
     stroke("black");
-    line(100,100,200,300);
+    line(85,75,30,75);
     arc(150,200,50,100, 0, PI+QUARTER_PI, OPEN);
     arc(250,200,50,100, 0, PI+QUARTER_PI, OPEN);
     
