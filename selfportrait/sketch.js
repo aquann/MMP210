34b4d36cd8df7fc4d1464ca026ee9e7c64d23f70
aquann <alexander.quann@f1006-30-34172.bmcc.cuny.edu>
2017-09-07 21:31:40 -0400

    var added

--- selfportrait/sketch.js
+++ selfportrait/sketch.js
@@ -1,25 +1,31 @@
 function setup() {
 createCanvas (640, 460);
 }
+
 function draw() {
+    
+    var faceColor = color("saddlebrown");
+    var eyelidColor = color(180, 50, 250, 127);
+    var mouth = 
     background("white"); 
     noStroke();
-    fill("saddlebrown");
     
-    ellipse(125, 220, 300, 350) ;
+    /*face color*/
+    fill(faceColor); //color for the face
+    ellipse(125, 220, 300, 350); // shape for the face
     
-    fill(180, 50, 250, 127)
+    // eyes
+    fill(eyelidColor);
+    
     ellipse(150,200,50,100);
     ellipse(250,200,50,100);
     
     fill(200)   
-    
-
     stroke("black");
     line(85,75,30,75);
+    
     arc(150,200,50,100, 0, PI+QUARTER_PI, OPEN);
     arc(250,200,50,100, 0, PI+QUARTER_PI, OPEN);
-    
-    
 
 }
+
