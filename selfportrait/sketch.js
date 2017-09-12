05b211b0cafc9d503cc9c5e179be1e2cd08569a7
aquann <31600145+aquann@users.noreply.github.com>
2017-09-12 19:51:07 -0400

    var change

--- selfportrait/sketch.js
+++ selfportrait/sketch.js
@@ -6,25 +6,37 @@ function draw() {
     
     var faceColor = color("saddlebrown");
     var eyelidColor = color(180, 50, 250, 127);
-    var mouth = 
-    background("white"); 
-    noStroke();
+    var lefteye = (150,200,50,100);
+    var righteye = (250,200,50,100);
+    var eyelidright = (150,200,50,100, 0, PI+QUARTER_PI, OPEN);
+    var eyelidleft = (250,200,50,100, 0, PI+QUARTER_PI, OPEN);
+    background("white");
     
-    /*face color*/
+    //face color
+    noStroke();
     fill(faceColor); //color for the face
     ellipse(125, 220, 300, 350); // shape for the face
     
     // eyes
     fill(eyelidColor);
     
+    // lefteye
     ellipse(150,200,50,100);
+    
+    //righteye
     ellipse(250,200,50,100);
     
     fill(200)   
     stroke("black");
-    line(85,75,30,75);
+    line(50,75,30,75);
     
+    fill(51);
+rect(25, 25, 100, 100);
+    
+    //eyelidright
     arc(150,200,50,100, 0, PI+QUARTER_PI, OPEN);
+   
+    //eyelidleft
     arc(250,200,50,100, 0, PI+QUARTER_PI, OPEN);
 
 }
